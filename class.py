9de2623da4c8f8e07936285e05806783efe83c56
swairaimran02-web <swairaimran02@gmail.com>
2026-03-31 20:25:01 +0500

    my games
    game

---
+++ class.py
@@ -0,0 +1,7 @@
+class userProfile:
+    def firstName(self,FirstName):
+        print(FirstName)
+    def lastName(self,LastName):
+        print(LastName)
+    def FullName(self,FirstName,LastName):
+        print(FirstName,LastName)
